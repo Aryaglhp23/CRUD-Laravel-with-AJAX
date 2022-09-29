aa4ca2c66b82953af3cb9bd66d2abdacf1ea762f
Aryaglhp23 <aryagalihpranowo22@gmail.com>
2022-09-29 19:58:03 +0700

    add insert data

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -19,5 +20,32 @@ class PostController extends Controller
 
         //return view with data
         return view('posts', compact('posts'));
+    }
+
+    public function store(Request $request) 
+    {
+        // make Validation
+        $validator= Validator::make($request->all(), [
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        // check Validation
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // create Post
+        $post = Post::create([
+            'title' => $request->title,
+            'content' => $request->content
+        ]);
+
+        // return Respons JSON
+        return response()->json([
+            'success' => true,
+            'message' => 'Data Berhasil Disimpan!',
+            'data'    => $post 
+        ]);
     }
 }
