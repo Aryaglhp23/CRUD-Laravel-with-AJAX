d9db1827bb1c6e22a25997ca62fd6784f2cd3d5c
Aryaglhp23 <aryagalihpranowo22@gmail.com>
2022-09-29 14:55:08 +0700

    Adding project

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * index
+     *
+     * @return void
+     */
+    public function index()
+    {
+        //get all posts from Models
+        $posts = Post::latest()->get();
+
+        //return view with data
+        return view('posts', compact('posts'));
+    }
+}
